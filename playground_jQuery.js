774ba51766d0fd2c4c4d3263bd740488bbeaae4f
ewen reynolds <ewen.reynolds@gmail.com>
2023-07-20 14:58:16 +0100

    Added playground areas.

---
+++ playground_jQuery.js
@@ -0,0 +1,23 @@
+
+
+// let doc = document.querySelector("h1");
+// doc.classList.add("uppercase");
+
+// Get a list of elements using class querySelect.
+let elements = $("h1");
+const firstH1 = elements[0];
+// firstH1.classList.add("uppercase");
+elements.text("This text was modified from jQuery script");
+
+// Get a list of elements using id querySelect.
+elements = $("#myh2");
+elements.css("color", "red");
+
+// Get a list of elements using style querySelect.
+elements = $(".pImportant");
+elements.css("font-weight", "Bold");
+
+// Get a list of elements using nested querySelect.
+elements = $("div .pImportant");
+elements.css("color", "blue");
+
